833a1dd55f7d1bf3380ae5313931f2b2e0e572fe
Mohd Fareed Subhani <Mohd.Fareed.Subhani@ibm.com>
2022-12-03 00:51:52 +0900

    project intial setup

---
+++ src/main/kotlin/com/ecommerce/salespoint/SalespointApplication.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.salespoint
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalespointApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalespointApplication>(*args)
+}
---
+++ src/test/kotlin/com/ecommerce/salespoint/SalespointApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ecommerce.salespoint
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalespointApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
